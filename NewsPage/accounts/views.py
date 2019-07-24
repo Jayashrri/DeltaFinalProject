270781106db3415fb2d001612c748e350ef112af
jayashrri <balaji.jayashrri@gmail.com>
2019-07-24 23:16:26 +0530

    Modified Sign Up Form

--- NewsPage/accounts/views.py
+++ NewsPage/accounts/views.py
@@ -1,30 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
-from django import forms
-from .forms import UserRegistrationForm
 from newspage.models import UserPreferences
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             userObj = form.cleaned_data
             username = userObj['username']
-            password =  userObj['password']
-            if not (User.objects.filter(username=username).exists()):
-                User.objects.create_user(username=username, password=password)
-                NewUser=User.objects.get(username=username)
-                NewPref=UserPreferences(user_name=NewUser, display_period=2, articles_per_page=10)
-                NewPref.save()
-                return HttpResponseRedirect('/accounts/login')    
-            else:
-                raise forms.ValidationError('User with that username already exists.')
-                
+            form.save()
+            NewUser=User.objects.get(username=username)
+            NewPref=UserPreferences(user_name=NewUser)
+            NewPref.save()
+            return HttpResponseRedirect('/accounts/login')                    
     else:
-        form = UserRegistrationForm()
+        form = UserCreationForm()
         
     return render(request, 'registration/signup.html', {'form' : form})
